70371e473a75cd4a1e4e093acc24f3009393ee0a
PhamQuyTung <pqthp18903@gmail.com>
2025-03-31 16:32:45 +0700

    Prettier - Code formatter Test 3

--- src/index.js
+++ src/index.js
@@ -1,30 +1,35 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const { engine } = require('express-handlebars');
+const path = 
+require('path')
+const express = 
+require('express')
+const morgan = 
+require('morgan')
+const { engine } = 
+require('express-handlebars')
 
-const app = express();
+const app = express()
 
-const route = require('./routes');
+const route = 
+require('./routes')
 
 //
-app.use(express.static(path.join(__dirname, 'public')));
+            app.use(express.static(path.join(__dirname, 'public')))
 
 // Middleware to parse JSON request bodies
-app.use(express.json());
-app.use(
-      express.urlencoded({
-            extended: true,
-      }),
-);
+            app.use(express.json());
+            app.use(
+                  express.urlencoded({
+                        extended: true,
+                  }),
+            );
 
 // HTTP logger
       app.use(morgan('combined'));
 
 // Cấu hình Handlebars với đuôi `.hbs`
-app.engine('hbs', engine({ extname: '.hbs' }));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+                  app.engine('hbs', engine({ extname: '.hbs' }));
+                  app.set('view engine', 'hbs');
+                  app.set('views', path.join(__dirname, 'resources/views'));
 
 console.log('Views path:', path.join(__dirname, 'resources/views'));
 
