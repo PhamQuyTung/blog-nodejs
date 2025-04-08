8ddb8b117f3c32ac0fea212056202eae8db48cd0
PhamQuyTung <pqthp18903@gmail.com>
2025-04-09 01:39:35 +0700

    Thêm phân trang và Logic nút "Ẩn bớt" & "Xem thêm"

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,21 +4,59 @@
 const Course = require('../models/Course');
 
 class MeController {
-    // [GET] /me/stored/courses
     storedCourses(req, res, next) {
-        // Sử dụng Course.find() để lấy tất cả các khóa học từ cơ sở dữ liệu
-        Course.find({})
-           .lean() // Chuyển đ��i dữ liệu thành Plain Object để Handlebars có thể xử lý
-           .then((courses) => {
-               res.render('me/stored-courses', { courses });
-           })
-           .catch(next);
-    }
+        const limit = 5;
+        const page = parseInt(req.query.page) || 1;
+        const sortQuery = req.query.sort;
 
-    // // [GET] /me/trash/courses
-    // trashCourses(req, res) {
-    //     res.render('me/trash-courses');
-    // }
+        const mapLevelToIndex = {
+            'Dễ': 1,
+            'Trung Bình': 2,
+            'Khó': 3,
+            'Pro': 4,
+        };
+
+        Course.find({})
+            .lean()
+            .then((allCourses) => {
+                // Sắp xếp theo query
+                if (sortQuery === 'level-asc') {
+                    allCourses.sort((a, b) => mapLevelToIndex[a.level] - mapLevelToIndex[b.level]);
+                } else if (sortQuery === 'level-desc') {
+                    allCourses.sort((a, b) => mapLevelToIndex[b.level] - mapLevelToIndex[a.level]);
+                } else if (sortQuery === 'price-asc') {
+                    allCourses.sort((a, b) => a.cost - b.cost);
+                } else if (sortQuery === 'price-desc') {
+                    allCourses.sort((a, b) => b.cost - a.cost);
+                } else if (sortQuery === 'name-asc') {
+                    allCourses.sort((a, b) => a.name.localeCompare(b.name));
+                } else if (sortQuery === 'name-desc') {
+                    allCourses.sort((a, b) => b.name.localeCompare(a.name));
+                } else if (sortQuery === 'oldest') {
+                    allCourses.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+                } else {
+                    allCourses.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)); // newest
+                }
+
+                const totalCourses = allCourses.length;
+                const totalPages = Math.ceil(totalCourses / limit);
+
+                const paginatedCourses = allCourses.slice(
+                    (page - 1) * limit,
+                    page * limit
+                );
+
+                res.render('me/stored-courses', {
+                    courses: paginatedCourses,
+                    currentPage: page,
+                    totalPages,
+                    limit,
+                    query: { sort: sortQuery },
+                });
+            })
+            .catch(next);
+    }
 }
 
 module.exports = new MeController();
+
--- src/utils/handlebars.js
+++ src/utils/handlebars.js
@@ -4,8 +4,40 @@ module.exports = {
     formatDate: (date) => {
         return moment(date).format('DD/MM/YYYY - HH:mm:ss');
     },
-    
+
     inc: function (value) {
         return parseInt(value) + 1;
+    },
+
+    dec: function (value) {
+        return parseInt(value) - 1;
+    },
+
+    gt: function (a, b) {
+        return a > b;
+    },
+
+    lt: function (a, b) {
+        return a < b;
+    },
+
+    ifEquals: function (a, b, options) {
+        return a === b ? options.fn(this) : options.inverse(this);
+    },
+
+    range: function (from, to) {
+        let arr = [];
+        for (let i = from; i <= to; i++) {
+            arr.push(i);
+        }
+        return arr;
+    },
+
+    indexPagination: function (index, currentPage, limit) {
+        return (currentPage - 1) * limit + index + 1;
+    },
+    
+    ifEquals: (a, b, options) => {
+        return a === b ? options.fn(this) : options.inverse(this);
     },
 };
